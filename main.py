34c2a545d53ee8d763abeff6791e7e2fd8a2781d
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-09-12 23:22:31 +1000

    Updated format of setup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import discord
 
+def extract_key():
+    f = open("api.key", "r")
+    return f.read().replace("DiscordAPI-Key =", "")
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -12,7 +16,12 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = MyClient(intents=intents)
-client.run('token')
+
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = MyClient(intents=intents)
+    client.run(extract_key())
+
+    
+    

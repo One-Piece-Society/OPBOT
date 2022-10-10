f03ebb1c8e96559c7c14d2ae8152be785c395718
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-10-10 23:06:34 +1100

    Added image module cog

--- cogs/imageModule.py
+++ cogs/imageModule.py
@@ -1,14 +1,39 @@
 import discord
 from discord.ext import commands
+from dataGetter import *
 
 
 class image(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name="ping")
-    async def ping_tester(self, ctx):
-        await ctx.channel.send('pong')
+    @commands.command(name="rimage")
+    async def random_image(self, ctx):
+        imageName = randomFile()
+        image = openImageData(imageName)
+        charName = cleanName(imageName)
+
+        info = openCharData(charName)
+
+        embedVar = discord.Embed(title=charName, color=0xed8b02)
+        embedVar.set_image(url="attachment://image.jpg")
+
+        await ctx.channel.send(file=image, embed=embedVar)
+
+    @commands.command(name="rdata")
+    async def random_image_data(self, ctx):
+        imageName = randomFile()
+        image = openImageData(imageName)
+        charName = cleanName(imageName)
+
+        info = openCharData(charName)
+
+        embedVar = discord.Embed(
+            title=f"{charName} - {info['Japanese Name']}", color=0xed8b02)
+        embedVar.set_image(url="attachment://image.jpg")
+
+        embedVar = addAllData(embedVar, info)
+        await ctx.channel.send(file=image, embed=embedVar)
 
 
 async def setup(client):

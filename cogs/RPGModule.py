b7a4d20622a6609bbdc4f87fcc5c77f5c67fd789
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-10-14 23:58:02 +1100

    added profile stub

--- cogs/RPGModule.py
+++ cogs/RPGModule.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 # from dataGetter import *
 from PIL import Image
+import io
 
 
 class RPG(commands.Cog):
@@ -64,7 +65,7 @@ class RPG(commands.Cog):
         image.thumbnail(MAX_SIZE)
         # image.save('temp.jpg')
 
-        image.show()
+        # image.show()
         # image2.show() 
         
         # print("test2")
@@ -73,19 +74,57 @@ class RPG(commands.Cog):
         # await ctx.channel.send(file=picture)
         # print("test3")
         
+        # print(2)
+        # buf = io.BytesIO()
+        # image.save(buf, format='JPEG')
+        # byte_im = buf.getvalue()
+        # print(2)
+
+
+        # image = discord.File(byte_im, filename="image.jpg")
+        # print(3)
+        # # picture = discord.File(f, filename="image.jpg")
         
-        
-        image = discord.File(image, filename="image.jpg")
-        print(3)
-        
-        embedVar = discord.Embed(title="asdasdasd", color=0xed8b02)
-        print(4)
+        # embedVar = discord.Embed(title="asdasdasd", color=0xed8b02)
+        # print(4)
     
+        # embedVar.set_image(url="attachment://image.jpg")
+        # print(5)
+
+        # await ctx.channel.send(file=byte_im, embed=embedVar)
+        
+        
+        
+        imageName = randomFile()
+        image = openImageData(imageName)
+        charName = cleanName(imageName)
+
+        info = openCharData(charName)
+
+        embedVar = discord.Embed(title=charName, color=0xed8b02)
         embedVar.set_image(url="attachment://image.jpg")
-        print(5)
 
         await ctx.channel.send(file=image, embed=embedVar)
-
+        
+    @commands.command(name="profile")
+    async def random_imassge(self, ctx):
+        """
+        Gets your profile
+        
+        Description
+        ___________________________________
+        Print out stats for your profile.
+        
+        Usage
+        ___________________________________
+        op!profile 
+        """
+        
+        await ctx.channel.send("getpass.getuser()")
+        await ctx.channel.send(ctx.author.id)
+        await ctx.channel.send(ctx.author.name)
+        
+        
 
 async def setup(client):
     await client.add_cog(RPG(client))

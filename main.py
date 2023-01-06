374465f53392b132d18e4714db7cbd416fe6fca7
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-06 23:52:39 +1100

    Modifed config file usage

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import asyncio
 import os
 import discord
 from discord.ext import commands
+import configparser
 
 
 def extract_key():
-    f = open("api.key", "r")
-    return f.read().replace("DiscordAPI-Key =", "")
-
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    return config['keys']['DiscordAPI-Key']
 
 async def main():
+    
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
 
-    # # client = commands.Bot(intents=intents, command_prefix=["op!","Op!"])
+    # client = commands.Bot(intents=intents, command_prefix=["op!","Op!"])
     client = commands.Bot(intents=intents, command_prefix=["opt!"])
     async with client:
         for cogFile in os.listdir("cogs"):
-            print(cogFile)
             if cogFile.endswith(".py"):
                 await client.load_extension(f'cogs.{cogFile[:-3]}')
 

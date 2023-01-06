eb28760d57ab0b3bc71a68f3c195c0d15fa26f7a
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-06 22:49:10 +1100

    Added ini files

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ async def main():
     intents.message_content = True
     intents.members = True
 
-    # client = commands.Bot(intents=intents, command_prefix=["op!","Op!"])
+    # # client = commands.Bot(intents=intents, command_prefix=["op!","Op!"])
     client = commands.Bot(intents=intents, command_prefix=["opt!"])
     async with client:
         for cogFile in os.listdir("cogs"):
+            print(cogFile)
             if cogFile.endswith(".py"):
                 await client.load_extension(f'cogs.{cogFile[:-3]}')
 
         await client.start(extract_key())
-
 
 asyncio.run(main())

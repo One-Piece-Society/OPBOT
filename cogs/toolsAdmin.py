0554a2176b160a8891ee61c343bf5458f61e60f9
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-06 22:25:40 +1100

    Added fucntion to get user id

--- cogs/toolsAdmin.py
+++ cogs/toolsAdmin.py
@@ -7,18 +7,18 @@ class admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(name="verifyGFWebHKPPServer2022")
-    async def verify_ping(self, ctx):
-        await ctx.channel.send('verifyiing')
-        await ctx.channel.send(ctx.channel.id)
+    # @commands.command(name="verifyGFWebHKPPServer2022")
+    # async def verify_ping(self, ctx):
+    #     await ctx.channel.send('verifyiing')
+    #     await ctx.channel.send(ctx.channel.id)
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if ctx.webhook_id == 1034044078821736458 and "Auth!verifyUserWebServer1.1" in ctx.message:
-            modChan = self.client.get_channel(790513461465317386)
-            print(type(modChan))
+        if ctx.webhook_id == 1034044078821736458 and "Auth!verifyUserWebServer1.1" in ctx.content:
+
+            print(ctx.content)
+            
             await ctx.channel.send("received")
-            await modChan.send("testing message being sent from anoter server")
 
     @commands.command(name="findid")
     async def find_id(self, ctx, name):

0aeb459ba1ecbe739dc96a3d5c88755408af4bb5
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-15 22:58:34 +1100

    Removed find if usgae

--- cogs/toolsAdmin.py
+++ cogs/toolsAdmin.py
@@ -57,66 +57,66 @@ class admin(commands.Cog):
             await ctx.channel.send(infoMessage)
             
 
-    @commands.command(name="findid")
-    async def find_id(self, ctx, name):
-        uname = "HUM#3250"
-        print(uname)
-        print(discord.version_info)
-        print(type(ctx))
-        print(type(self.client))
-        # user = ctx.users.find("username", "TESTname");
+    # @commands.command(name="findid")
+    # async def find_id(self, ctx, name):
+    #     uname = "HUM#3250"
+    #     print(uname)
+    #     print(discord.version_info)
+    #     print(type(ctx))
+    #     print(type(self.client))
+    #     # user = ctx.users.find("username", "TESTname");
 
-        # guild = ctx.guild
-        # print("break0")
-        # print(guild)
-        # print(discord.ctx(users))
-        # for guild in ctx.guilds:
-        #     print("mem")
-        #     for member in guild.members:
-        #         print("in")
+    #     # guild = ctx.guild
+    #     # print("break0")
+    #     # print(guild)
+    #     # print(discord.ctx(users))
+    #     # for guild in ctx.guilds:
+    #     #     print("mem")
+    #     #     for member in guild.members:
+    #     #         print("in")
 
-        # print(self.client.users)
-        # # print("----------")
-        # print(self.client.get_all_channels())
+    #     # print(self.client.users)
+    #     # # print("----------")
+    #     # print(self.client.get_all_channels())
 
-        channel = self.client.get_channel(1018874117870583828)
-        await channel.send("Hello, world!")
+    #     channel = self.client.get_channel(1018874117870583828)
+    #     await channel.send("Hello, world!")
 
-        # print(channel.members)
-        # print(channel.members[0])
+    #     # print(channel.members)
+    #     # print(channel.members[0])
 
-        # print(channel.members[1])
-        # print(name)
-        for name1 in channel.members:
-            # print(type(name1))
-            if str(name1) == name:
-                # print("found result of user")
-                # print(type(name1))
-                await channel.send(f'<@{name1.id}> pong!')
+    #     # print(channel.members[1])
+    #     # print(name)
+    #     for name1 in channel.members:
+    #         # print(type(name1))
+    #         if str(name1) == name:
+    #             # print("found result of user")
+    #             # print(type(name1))
+    #             await channel.send(f'<@{name1.id}> pong!')
 
-                # role = discord.utils.get(self.client.get_channel(1018874117870583828).roles, name='x1xx1')
-                print("new line ")
-                guild = self.client.get_guild(680709749960081427)
+    #             # role = discord.utils.get(self.client.get_channel(1018874117870583828).roles, name='x1xx1')
+    #             print("new line ")
+    #             guild = self.client.get_guild(680709749960081427)
 
-                print("asd")
-                print(guild.roles)
-                role = discord.utils.get(guild.roles, name='xxx1')
+    #             print("asd")
+    #             print(guild.roles)
+    #             role = discord.utils.get(guild.roles, name='xxx1')
 
-                # print(role)
-                print(type(role))
+    #             # print(role)
+    #             print(type(role))
 
-                await name1.remove_roles(role)
+    #             await name1.remove_roles(role)
 
-        # print(f"error user "{name}" not found or needs manual verification ")
-        # print(ctx.channel.id)
+    #     # print(f"error user "{name}" not found or needs manual verification ")
+    #     # print(ctx.channel.id)
 
-        # user_id = await find_user_id(guild, name)
-        # print("break1")
+    #     # user_id = await find_user_id(guild, name)
+    #     # print("break1")
 
-        # if user_id:
-        #     await ctx.channel.send(f'The user ID of {name} is {user_id}')
-        # else:
-        #     await ctx.channel.send(f'No user was found with the name {name}')
+    #     # if user_id:
+    #     #     await ctx.channel.send(f'The user ID of {name} is {user_id}')
+    #     # else:
+    #     #     await ctx.channel.send(f'No user was found with the name {name}')
 
 
 async def setup(client):

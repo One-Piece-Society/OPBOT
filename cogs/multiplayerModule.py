735a70a81e7984189b10b72c60c0b50fc341d1b8
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-27 00:09:26 +1100

    Added basic core for multiplayer mod

--- cogs/multiplayerModule.py
+++ cogs/multiplayerModule.py
@@ -1,15 +1,24 @@
 import discord
 from discord.ext import commands
 from dataGetter import *
-from PIL import Image
-import io
-import json
-import time
-import random
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 class Multiplayer(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.activeGames = []
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if config['testing']['DisableMultiplayer'] == "true":
+            return
+
+        print("ceas")
+    
+    
 
 async def setup(client):
     await client.add_cog(Multiplayer(client))

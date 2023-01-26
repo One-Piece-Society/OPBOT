b2b1d7ad457194db4e79b4b10b725e5ae96a47f3
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-27 00:55:41 +1100

    Added multiplayer modules

--- cogs/multiplayerModule.py
+++ cogs/multiplayerModule.py
@@ -9,16 +9,39 @@ config.read('config.ini')
 class Multiplayer(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.activeGames = []
+        self.activeGames = {}
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if config['testing']['DisableMultiplayer'] == "true":
             return
 
-        print("ceas")
-    
-    
+    @commands.command(name="start")
+    async def daily_prompt(self, ctx):
+        """
+        A simple guess the character quiz
+
+        Description
+        ___________________________________
+        Fun multiplayer game to compete with your "friends"
+
+        Usage
+        ___________________________________
+        op!start
+        """
+        embed = discord.Embed(color=0xff8800)
+
+        if ctx.channel.id in self.activeGames:
+            embed.add_field(name=f"There is already a game started here", value="", inline=True)
+        else: 
+            embed.add_field(name=f"Quiz session has started", value="", inline=False)
+            embed.add_field(name=f"", value="", inline=False)
+            embed.add_field(name=f"Game will begin in 30 secs", value="Click the tick to join", inline=False)
+
+
+        
+        await ctx.channel.send(embed=embed)
+
 
 async def setup(client):
     await client.add_cog(Multiplayer(client))

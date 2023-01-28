6a1f20ffac51a01ae3d6457e07b81b4d27f52731
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-28 23:44:05 +1100

    Added function to display results test

--- cogs/multiplayerModule.py
+++ cogs/multiplayerModule.py
@@ -65,7 +65,6 @@ class Multiplayer(commands.Cog):
 
         await ctx.channel.send(embed=embed)
 
-
     @commands.command(name="startquiz")
     async def pictureGame_prompt(self, ctx):
         """
@@ -96,7 +95,7 @@ class Multiplayer(commands.Cog):
         sentMsg = await ctx.channel.send(embed=embed)
         await sentMsg.add_reaction('✅')
 
-        self.activeGames[str(ctx.channel.id)] = {"state": "joining"}
+        self.activeGames[str(ctx.channel.id)] = {"state": 0}
         print(self.activeGames)
 
         time.sleep(2)
@@ -106,59 +105,87 @@ class Multiplayer(commands.Cog):
         message = await ctx.fetch_message(sentMsg.id)
         usersjoined = [user async for user in message.reactions[0].users() if user.id != sentMsg.author.id]
 
-        # Sends game start confirmation 
+        # Sends game start confirmation
         embed = discord.Embed(color=0xa903fc)
         if len(usersjoined) == 0:
             embed.add_field(name=f"No one has joined",
-                value="The game will cease to run", inline=False)
+                            value="The game will cease to run", inline=False)
             await ctx.channel.send(embed=embed)
             del self.activeGames[str(ctx.channel.id)]
             return
-        
-        
+
         # elif len(usersjoined) == 1:
         #     embed.add_field(name=f"Seams no one wants to join yet",
         #         value="try again when there are more people (solo feature to be added later)", inline=False)
         #     await ctx.channel.send(embed=embed)
         #     del self.activeGames[str(ctx.channel.id)]
         #     return
-        
+
         else:
             embed.add_field(name=f"Players joined",
                             value="The game will start in 10 secs", inline=False)
             embed.add_field(name=f"",
-                value="50/50 and skips can be used every 5 rounds", inline=False)
+                            value="50/50 and skips can be used every 5 rounds", inline=False)
 
             self.activeGames[str(ctx.channel.id)]["state"] = 1
             self.activeGames[str(ctx.channel.id)]["players"] = {}
             for user in usersjoined:
                 self.activeGames[str(ctx.channel.id)]["players"][str(user.id)] = {
+                    "name": user.name[:10],
                     "skip": 0,
                     "50": 0,
                     "health": 3,
                 }
 
-                embed.add_field(name=f"{user.name}",value="", inline=False)
+                embed.add_field(name=f"{user.name}", value="", inline=False)
 
             await ctx.channel.send(embed=embed)
 
-
-                
         print(self.activeGames)
 
+        # for reaction in message.reactions:
+        #     print(f'{reaction.emoji} has been used {reaction.count} times')
 
-
-            # for reaction in message.reactions:
-            #     print(f'{reaction.emoji} has been used {reaction.count} times')
-
-            # print(sentMsg.id)
-            # print(str(sentMsg.reactions))
-            # for react in sentMsg.reactions:
-            #     print(react)
+        # print(sentMsg.id)
+        # print(str(sentMsg.reactions))
+        # for react in sentMsg.reactions:
+        #     print(react)
 
         await ctx.channel.send("timer up")
 
-            # threading.Thread(target=startServer, args=mainCog).start()
+        # threading.Thread(target=startServer, args=mainCog).start()
+
+    @commands.command(name="test1")
+    async def screen1_prompt(self, ctx):
+        roundNo = 12
+        answerName = "luffy"
+        answerValue = "A"
+        playerInfo = {'279119312072081408': {'name': "alex", 'skip': 0, '50': 0, 'health': 3, "previousAns": "C"},
+                      '279119312072081409': {'name': "alex2", 'skip': 5, '50': 2, 'health': 1, "previousAns": "50"},
+                      '279119312072081411': {'name': "alex4", 'skip': 5, '50': 2, 'health': 0, "previousAns": "None"},
+                      '279119312072081410': {'name': "alex3wwwww", 'skip': 1, '50': 3, 'health': 3, "previousAns": "skip"}}
+
+        embed = discord.Embed(color=0xff8800)
+
+        embed.add_field(name=f"Round results",
+                        value=f"The correct answer was :regional_indicator_{answerValue.lower()}: {answerName}", inline=False)
+        embed.add_field(name=f"", value="", inline=False)
+        embed.add_field(
+            name=f"Name (:hearts: Health / :negative_squared_cross_mark: 50/50 / :fast_forward: Skips / :capital_abcd: Answer )", value="", inline=False)
+
+        for idx in playerInfo:
+            name = playerInfo[idx]['name']
+            hearts = ":hearts:" * playerInfo[idx]['health']
+            skips = playerInfo[idx]['skip']
+            halfs = playerInfo[idx]['50']
+            givenAns = playerInfo[idx]['previousAns']
+
+            embed.add_field(name=f"", value=f"{name}      ( {hearts} / {skips} / {halfs} / {givenAns})", inline=False)
+
+            # embed.add_field(
+            #     name=f"", value=f":heart:", inline=False)
+
+        await ctx.channel.send(embed=embed)
 
 
 async def setup(client):

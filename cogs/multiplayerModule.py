67b4735da70c1b2374b4164226a2dd38c6cf5a68
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-01-28 22:05:24 +1100

    Added player modes

--- cogs/multiplayerModule.py
+++ cogs/multiplayerModule.py
@@ -45,9 +45,9 @@ class Multiplayer(commands.Cog):
             embed.add_field(name=f"", value="", inline=False)
             embed.add_field(name=f"Game will begin in 30 secs",
                             value="Click the tick to join", inline=False)
-
             sentMsg = await ctx.channel.send(embed=embed)
             await sentMsg.add_reaction('✅')
+
             self.activeGames[str(ctx.channel.id)] = {"state": "joining"}
             print(self.activeGames)
 
@@ -55,7 +55,27 @@ class Multiplayer(commands.Cog):
             await sentMsg.add_reaction('⏱️')
             time.sleep(1)
 
+            message = await ctx.fetch_message(sentMsg.id)
+            
+            print(message.reactions)
+            print(type(message.reactions[0]))
+            tick = message.reactions[0]
+
+            async for user in tick.users():
+                await ctx.channel.send(f'{user} has reacted with {tick.emoji}!')
+
+
+            # for reaction in message.reactions:
+            #     print(f'{reaction.emoji} has been used {reaction.count} times')
+
+
+            # print(sentMsg.id)
+            # print(str(sentMsg.reactions))
+            # for react in sentMsg.reactions:
+            #     print(react)
+
             await ctx.channel.send("timer up")
+            
 
             # threading.Thread(target=startServer, args=mainCog).start()
 
